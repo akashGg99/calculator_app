7d4c56987679f63f3c4f66b7a0ba62253ef388ab
akashGg99 <aakkash15r@gmail.com>
2023-03-31 22:15:46 +0530

    component fixes

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,33 +1,32 @@
 
-export default function Calculator(){
+export default function Calculator() {
 
-    return(
+    return (
 
         <div>
-        <hi>Calulator</hi>
-        <input  value={userInput}></input>
-        <br></br>
-        <p>Answer : </p>
-        <br></br>
-        <CalcButton number={1} />
-        <CalcButton number={2} />
-        <CalcButton number={3} />
-        <CalcButton number={"+"} />
-        <br/>
-        <CalcButton number={4} />
-        <CalcButton number={5} />
-        <CalcButton number={6} />
-        <CalcButton number={"-"} />
-        <br/>
-        <CalcButton number={7} />
-        <CalcButton number={8} />
-        <CalcButton number={9} />
-        <CalcButton number={"X"} />
-        <br/>
-        <CalcButton number={"."} />
-        <CalcButton number={0} />
-        <CalcButton number={"/"} />
-        <CalcButton number={"="} />
-    </div>
+            <h1>Calulator</h1>
+            <input ></input>
+            <br></br>
+            <br></br>
+            <button value={"1"} >1</button>
+            <button value={"2"} >2</button>
+            <button value={3} >3</button>
+            <button value={"+"} >+</button>
+            <br />
+            <button value={4} >4</button>
+            <button value={5} >5</button>
+            <button value={6} >6</button>
+            <button value={"-"} >-</button>
+            <br />
+            <button value={7} >7</button>
+            <button value={8} >8</button>
+            <button value={9} >9</button>
+            <button value={"X"} >x</button>
+            <br />
+            <button value={"."} >.</button>
+            <button value={0} >0</button>
+            <button value={"/"} >/</button>
+            <button value={"="} >=</button>
+        </div>
     )
 }
