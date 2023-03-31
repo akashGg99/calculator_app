3834695186a9b92cde0b5cefc049ca6104ffcb2a
akashGg99 <aakkash15r@gmail.com>
2023-03-31 11:37:52 +0530

    added calculator component

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -2,6 +2,32 @@
 export default function Calculator(){
 
     return(
+
+        <div>
         <hi>Calulator</hi>
+        <input  value={userInput}></input>
+        <br></br>
+        <p>Answer : </p>
+        <br></br>
+        <CalcButton number={1} />
+        <CalcButton number={2} />
+        <CalcButton number={3} />
+        <CalcButton number={"+"} />
+        <br/>
+        <CalcButton number={4} />
+        <CalcButton number={5} />
+        <CalcButton number={6} />
+        <CalcButton number={"-"} />
+        <br/>
+        <CalcButton number={7} />
+        <CalcButton number={8} />
+        <CalcButton number={9} />
+        <CalcButton number={"X"} />
+        <br/>
+        <CalcButton number={"."} />
+        <CalcButton number={0} />
+        <CalcButton number={"/"} />
+        <CalcButton number={"="} />
+    </div>
     )
 }
